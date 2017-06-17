1cd0447f73c062b3e5ec8a89554ec981182bcfde
B. Heeren <b.heeren@zonnet.nl>
2017-06-17 23:08:27 +0200

    Signed-off-by: B. Heeren <b.heeren@zonnet.nl>

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ restService.post('/hook', function (req, res) {
             return res.json({
                 speech: speech,
                 displayText: speech,
-                parameters{"pam_total": PAM},
+ //               parameters{"pam_total": PAM},
                 source: 'smartsusan'
             });   
 
