ae6772ea466a6fc5eb51f535f2371d494b7bfed3
B. Heeren <b.heeren@zonnet.nl>
2017-06-30 19:49:59 +0200

    Signed-off-by: B. Heeren <b.heeren@zonnet.nl>

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ restService.post('/hook', function (req, res) {
                  speech += 'Paula actie: ' + requestBody.fulfillment.speech;
                  break;
             case "who_are_you": //check if user is known 
-                speech += 'Jij bent: ' + req.body.sessionId + '. ' + requestBody.fulfillment.speech;
+                speech += 'Jij bent: ' + '. ' + requestBody.fulfillment.speech;
                 //speech += requestBody.fulfillment.speech + 'Barend';
                 return res.json({
                     speech: speech,
