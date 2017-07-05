d2f0562ce377bbf218ae82d9fc481ad7aeae2ebc
B. Heeren <b.heeren@zonnet.nl>
2017-07-05 19:08:59 +0200

    Signed-off-by: B. Heeren <b.heeren@zonnet.nl>

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ restService.get('/', function (req, res) {
 });
 // Server frontpage
 restService.get('/hook', function (req, res) {
-    if (req.query['hub.verify_token'] === testbot_verify_token) {
+    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
         res.send(req.query['hub.challenge']);
     } else {
         res.send('Invalid verify token');
